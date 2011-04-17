a62729083083aa9e1f00f5dc133e6c50758b0106
eiji.ienaga <eiji.ienaga@gmail.com>
2011-04-17 09:01:35 +0900

    modify find logic

--- Support/lib/search_and_paste.rb
+++ Support/lib/search_and_paste.rb
@@ -1,18 +1,20 @@
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/ui"
 
+# config
 word = ENV['TM_CURRENT_WORD']
 find_dir = ENV['TM_PROJECT_DIRECTORY']
+extensions = %w[rb haml sass]
 
 if word.nil? || word.size <= 1
   puts "too short. please 2 more type(search keyword)"
   exit
 end
 
-results = `find "#{find_dir}" | egrep 'rb$|haml$|sass$|css$' | xargs egrep "#{word}" | sed 's/.*#{word}/#{word}/'|sort|uniq`
+names = "\\(#{extensions.map{|e| "-or -name '*.#{e}' "}.join}\\)".sub("-or", "")
+results = `find "#{find_dir}"  #{names} -print0 | xargs -0 egrep "#{word}" | sed 's/.*#{word}/#{word}/'|sort|uniq`
 
 choices = results.split("\n").map do |node|
   {'display' =>  node, 'match' => node}
 end
-TextMate::UI.complete(choices, :extra_chars => '_!?')
 
-#
+TextMate::UI.complete(choices, :extra_chars => '_!?') 
--- sandbox.rb
+++ sandbox.rb
@@ -1,7 +1,11 @@
 class QuickCopy
-  def copy_and_paste
+  def copy_and_paste(param)
+  end
+  def copy_and_paste2(param, param2)
   end
 end
+
+
 
 
 
